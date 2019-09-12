ba8816b026a328729a494914ffb1ddf8c9b8a652
sandeepkundala <sandeepkundala@gmail.com>
2019-09-11 21:17:46 -0700

    server exercise completed

--- server/index.js
+++ server/index.js
@@ -13,4 +13,28 @@ app.post('/', function (req, res) {
 
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// post JSON data using /share
+// create data structure which stores the file name with JSON body
+var path_dict= {};
+var pic_no=0
+app.post('/share', function (req, res) {
+   var file_name="/pgiPc".concat(String(pic_no));
+   path_dict[file_name]=req.body;
+   console.log(file_name);
+   console.log(pic_no);
+   res.send(`success:True,link:http://localhost:3000${file_name}`); 
+   pic_no+=1;
+});
+
+
+// Output: Print JSON or return ERROR
+app.get('/pgi*',function(req, res){
+   if (req.path in path_dict){
+    res.send(`${JSON.stringify(path_dict[req.path])}`);
+    delete path_dict[req.path];
+ } else{
+    res.send("success:false,error:404,message:Not Found");
+ }
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
